bd63d7b2fbd80ca0c0fd2a845d6550a5ad7bb4b2
Phil Howard <phil@gadgetoid.com>
2018-07-17 14:43:24 +0100

    Added set_multiple_pixels

--- library/ledshim/__init__.py
+++ library/ledshim/__init__.py
@@ -21,3 +21,26 @@ get_buffer_shape = display.get_shape
 get_shape = display.get_shape
 set_clear_on_exit = display.set_clear_on_exit
 
+def set_multiple_pixels(indexes, from_colour, to_colour=None):
+    """Set multiple pixels to a range of colours sweeping from from_colour to to_colour
+
+    :param from_colour: A tuple with 3 values representing the red, green and blue of the first colour
+    :param to_colour: A tuple with 3 values representing the red, green and blue of the second colour
+
+    """
+
+    if to_colour is None:
+        to_colour = from_colour
+
+    length = float(len(indexes))
+    step = 0
+    from_r, from_g, from_b = from_colour
+    to_r, to_g, to_b = to_colour
+    step_r, step_g, step_b = to_r - from_r, to_g - from_g, to_b - from_b
+    step_r /= length
+    step_g /= length
+    step_b /= length
+
+    for index in indexes:
+        set_pixel(index, from_r + (step_r * step), from_g + (step_g * step), from_b + (step_b * step))
+        step += 1
